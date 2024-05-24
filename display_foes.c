4df88688e83c7e3ded1816670b681167e2d01648
mrioshe42 <166904743+mrioshe42@users.noreply.github.com>
2024-05-24 14:48:59 +0200

    Create display_foes.c

---
+++ display_foes.c
@@ -0,0 +1,22 @@
+#include "so_long.h"
+
+void	display_foes(t_game *game, int frame)
+{
+	char	*files[3];
+	void	*img_ptr;
+	int		i;
+
+	files[0] = "textures/C4.xpm";
+	files[1] = "textures/C1.xpm";
+	files[2] = "textures/C3.xpm";
+	img_ptr = mlx_xpm_file_to_image(game->mlx, files[frame],
+			&game->img->width, &game->img->height);
+	i = 0;
+	while (i < game->num_foes)
+	{
+		mlx_put_image_to_window(game->mlx, game->win, img_ptr,
+			game->foes[i].x * PIXEL_SIZE, game->foes[i].y * PIXEL_SIZE);
+		i++;
+	}
+	mlx_destroy_image(game->mlx, img_ptr);
+}
